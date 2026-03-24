3076c552c69dbcd5d1601ef1f6244d8b395fc079
Kimmo Ahola @ Systementor <kimmo.ahola@systementor.se>
2026-03-24 11:54:21 +0100

    added shutil and pathlib usage

--- shutil_demo.py
+++ shutil_demo.py
@@ -1,3 +1,21 @@
 import shutil
+from pathlib import Path
 
-shutil.copy2("test.ini", "test.bak.ini")
+current_folder = Path().cwd()
+# Path.home() eller Path().home()
+
+copies_folder = current_folder / "copies"
+
+copies_folder.mkdir(exist_ok=True)
+
+
+
+shutil.copy(src="test.ini", dst=copies_folder / "test.bak.ini")
+shutil.copy2("test.ini", copies_folder / "test.bak.ini")
+shutil.move("test.bak.ini", copies_folder / "test.bak.ini")
+
+from shutil import copy, copy2, move
+
+copy(src="test.ini", dst=copies_folder / "test_2.bak.ini")
+copy2("test.ini", copies_folder / "test_2.bak.ini")
+# move()
